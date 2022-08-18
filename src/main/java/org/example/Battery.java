23fffee9892102603331a42c7a81baa52c83bf2f
Yazan Kittaneh <yazan.kittane@progressoft.com>
2022-08-18 11:17:26 +0200

    Initial commit

--- src/main/java/org/example/Battery.java
+++ src/main/java/org/example/Battery.java
@@ -1,2 +1,54 @@
-package org.example;public class Battery {
+package org.example;
+
+public class Battery {
+
+    // todo: consider adding charging and discharging efficiency
+
+    private long maxCapacity;
+    private long currentLoad;
+
+    public Battery() {
+        this(100L, 0L);
+    }
+
+    public Battery(long maxCapacity, long currentLoad) {
+        this.maxCapacity = maxCapacity;
+        this.currentLoad = currentLoad;
+    }
+
+    public long charge(long inputLoad) {
+        this.currentLoad += inputLoad;
+        if (this.currentLoad > this.maxCapacity) this.currentLoad = this.maxCapacity;
+        return this.currentLoad;
+    }
+
+    public long discharge(long outputLoad) {
+        this.currentLoad -= outputLoad;
+        if (this.currentLoad < 0) {
+            long lackingAmount = this.currentLoad;
+            this.currentLoad = 0;
+            return outputLoad + lackingAmount;
+        }
+        return outputLoad;
+    }
+
+    public boolean isFull() {
+        return this.maxCapacity == this.currentLoad;
+    }
+
+    public long getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(long maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public long getCurrentLoad() {
+        return currentLoad;
+    }
+
+    public void setCurrentLoad(long currentLoad) {
+        this.currentLoad = currentLoad;
+    }
 }
--- src/main/java/org/example/BatterySimulator.java
+++ src/main/java/org/example/BatterySimulator.java
@@ -1,2 +1,103 @@
-package org.example;public class BatterySimulator {
+package org.example;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.util.logging.Logger;
+
+public class BatterySimulator {
+
+    private static final Logger logger = Logger.getLogger(BatterySimulator.class.getName());
+
+    private static Battery battery;
+
+    private static Connection activeMqConnection;
+
+    private static Session activeMqSession;
+
+    private static MessageConsumer activeMqConsumer;
+
+    private static boolean keepAlive = true;
+
+    public static void main(String[] strings) {
+        logger.info("org.example.BatterySimulator started...");
+
+        battery = new Battery(100L, 0);
+
+        establishActiveMqConnection();
+
+        try {
+            while (keepAlive) {
+                Thread.sleep(500);
+
+                TextMessage message = (TextMessage) activeMqConsumer.receive();
+
+                if (message == null)
+                    continue;
+
+                String messageBody = message.getText();
+
+                logger.info("messageBody = " + messageBody);
+
+                handleMessage(messageBody);
+            }
+
+        } catch (InterruptedException | JMSException e) {
+            logger.warning("Error while reading activeMq message");
+            throw new RuntimeException(e);
+        }
+
+        closeActiveMqConnection();
+        logger.info("org.example.BatterySimulator finished");
+    }
+
+    private static void handleMessage(String messageBody) {
+        if ("print".startsWith(messageBody))
+            logger.info("received command to print");
+        else if ("charge".startsWith(messageBody)) {
+            battery.charge(extractValue(messageBody));
+            logger.info("battery charge = " + battery.getCurrentLoad());
+        } else if ("discharge".startsWith(messageBody)) {
+            battery.discharge(extractValue(messageBody));
+            logger.info("battery charge = " + battery.getCurrentLoad());
+        } else if ("shutdown".startsWith(messageBody))
+            keepAlive = false;
+        else
+            logger.warning("Unknown command received = " + messageBody);
+    }
+
+    private static long extractValue(String messageBody) {
+        return Long.parseLong(messageBody.split(":")[1]);
+    }
+
+    private static void establishActiveMqConnection() {
+        if (activeMqSession == null) {
+            logger.info("Establishing activeMq connection");
+
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+            try {
+                activeMqConnection = connectionFactory.createConnection();
+                activeMqConnection.start();
+
+                activeMqSession = activeMqConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                activeMqConsumer = activeMqSession.createConsumer(activeMqSession.createQueue("TESTFOO"));
+            } catch (JMSException e) {
+                logger.warning("Error while establishing activeMq connection");
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void closeActiveMqConnection() {
+        logger.info("Closing activeMq session and connection");
+        try {
+            if (activeMqSession != null)
+                activeMqSession.close();
+            if (activeMqConnection != null)
+                activeMqConnection.close();
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
